210fdfee9498ddc1bbb241fc3d4dc622b26ae820
Adrien Brignon <contact@abrignon.dev>
2019-05-11 13:58:03 +0200

    Now properly handling comments

--- src/readfile.c
+++ src/readfile.c
@@ -6,18 +6,24 @@
 */
 
 #include "redcode.h"
+#include "my/my_string.h"
 
 ssize_t readfile(FILE *fp, char **ptr)
 {
     size_t n = 0;
     ssize_t len = 0;
+    char *comment = NULL;
 
     if ((len = getline(ptr, &n, fp)) == EOF)
         return len;
     if (**ptr == '\n' || **ptr == '\0' || **ptr == '#')
         return readfile(fp, ptr);
+    if ((comment = my_strchr(*ptr, '#')) != NULL)
+        *comment = '\0';
 
     (*ptr)[len - 1] = '\0';
+
+    printf("%s\n", *ptr);
 
     return len;
 }
