40a5187d20701a52fd2b1fccaa383883a0b4c169
Lucas De Curtis <lucas.de-curtis@epitech.eu>
2019-05-11 21:56:56 +0200

    adding error handling for register

--- src/get_argument.c
+++ src/get_argument.c
@@ -7,6 +7,7 @@
 
 #include "redcode.h"
 #include "my/my_ctype.h"
+#include "my/my_stdlib.h"
 
 static char *extract_label(char *str)
 {
@@ -19,12 +20,20 @@ static char *extract_label(char *str)
     return (str);
 }
 
-static char *check_argument(char *str)
+static char *extract_argument(char *str)
 {
     if (str[0] == DIR_CHAR && (my_isdigit(str[1]) || str[1] == LAB_CHAR))
         return (str + 1);
-    if (str[0] != DIR_CHAR && (my_isdigit(str[1]) || str[1] == LAB_CHAR))
+    if (str[0] == REG_CHAR) {
+        if (my_atoi(&str[1]) > 0 && my_atoi(&str[1]) < 17)
+            return (str + 1);
+        else
+            return (NULL);
+    }
+    if (str[0] != DIR_CHAR && str[1] == LAB_CHAR)
         return (str + 1);
+    if (str[0] != DIR_CHAR && my_isdigit(str[1]))
+        return (str);
     return (NULL);
 }
 
@@ -37,11 +46,11 @@ argument_t get_argument(unsigned int types, const char *str)
     if (*str == DIR_CHAR && *(str + 1) == LAB_CHAR)
         return (argument_t) {T_LAB | T_DIR, IND_SIZE, extract_label((char *) str)};
     if (*str == REG_CHAR)
-        return (argument_t) {T_REG, REG_SIZE, str + 1};
+        return (argument_t) {T_REG, REG_SIZE, extract_argument((char *) str)};
     if (*str == DIR_CHAR && (types & T_SPE) == T_SPE)
-        return (argument_t) {T_DIR, IND_SIZE, check_argument((char *) str)};
+        return (argument_t) {T_DIR, IND_SIZE, extract_argument((char *) str)};
     if (*str == DIR_CHAR)
-        return (argument_t) {T_DIR, DIR_SIZE, check_argument((char *) str)};
+        return (argument_t) {T_DIR, DIR_SIZE, extract_argument((char *) str)};
 
-    return (argument_t) {T_IND, IND_SIZE, str};
+    return (argument_t) {T_IND, IND_SIZE, extract_argument((char *) str)};
 }
