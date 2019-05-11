ee8578b1415bc080fe142a6b6d5f49740240ccf3
Adrien Brignon <contact@abrignon.dev>
2019-05-11 15:04:19 +0200

    Trying with new padding

--- src/redcode_encode.c
+++ src/redcode_encode.c
@@ -23,14 +23,17 @@ static int encode_header(parser_t *parser)
     WRITE(parser, (uint32_t []) {REDCODE_HEADER}, 4, 1);
     WRITE(parser, name->value, 1, my_strlen(name->value));
 
-    for (size_t i = 0; i < NAME_LENGTH - (my_strlen(name->value) * 8); i++)
+    for (size_t i = my_strlen(name->value); i < NAME_LENGTH; i++)
         WRITE(parser, (uint8_t []) {0}, 1, 1);
 
+    WRITE(parser, (uint32_t []) {0}, 4, 1);
     WRITE(parser, (uint32_t []) {__bswap_32(parser->size)}, 4, 1);
     WRITE(parser, comment->value, 1, my_strlen(comment->value));
 
-    for (size_t i = 0; i < COMMENT_LENGTH - (my_strlen(comment->value) * 8) - 4; i++)
+    for (size_t i = my_strlen(comment->value); i < COMMENT_LENGTH; i++)
         WRITE(parser, (uint8_t []) {0}, 1, 1);
+
+    WRITE(parser, (uint32_t []) {0}, 4, 1);
 
     return 0;
 }
