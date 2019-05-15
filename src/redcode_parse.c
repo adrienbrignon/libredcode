0e2f789b46beb41b04bf07c7f99f626b04b0b6a1
Adrien Brignon <contact@abrignon.dev>
2019-05-15 09:23:56 +0200

    Small norm fixup

--- src/redcode_parse.c
+++ src/redcode_parse.c
@@ -7,6 +7,18 @@
 
 #include "redcode.h"
 
+static int push(parser_t *parser, instruction_t *ins, char *line)
+{
+    directive_t *directive = NULL;
+
+    if (ins->mnemonic.name != NULL || ins->label != NULL)
+        return list_push(parser->instructions, ins);
+    else if ((directive = parse_directive(line)) != NULL)
+        return list_push(parser->directives, directive);
+
+    return -1;
+}
+
 parser_t *redcode_parse(FILE *in)
 {
     char *line = NULL;
@@ -16,16 +28,10 @@ parser_t *redcode_parse(FILE *in)
         return NULL;
     while (readfile(parser->in, &line) != EOF) {
         instruction_t *ins = NULL;
-        directive_t *directive = NULL;
 
         if ((ins = parse_instruction(parser, line)) == NULL)
             return NULL;
-
-        if (ins->mnemonic.name != NULL || ins->label != NULL)
-            list_push(parser->instructions, ins);
-        else if ((directive = parse_directive(line)) != NULL)
-            list_push(parser->directives, directive);
-        else
+        if (push(parser, ins, line) < 0)
             return NULL;
     }
     return parser;
