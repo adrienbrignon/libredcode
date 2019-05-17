f20b7a70b2f75cfe2188d5ae86e405377ea88898
Adrien Brignon <contact@abrignon.dev>
2019-05-17 10:27:04 +0200

    Fixed wrong label encoding

--- src/encode_label.c
+++ src/encode_label.c
@@ -13,11 +13,10 @@ int encode_label(parser_t *parser, instruction_t *ins, size_t i)
 
     if (label == NULL)
         return -1;
-
-    WRITE(parser, (uint16_t []) {SWAP_16(label->offset - ins->offset)}, 2, 1);
-
     if (ins->argv[i].size == 4)
         WRITE(parser, (uint16_t []) {0}, 2, 1);
+
+    WRITE(parser, (uint16_t []) {SWAP_16(label->offset - ins->offset)}, 2, 1);
 
     return 0;
 }
